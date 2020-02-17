a1b71cb891a31f3b86c1f0ea50622c7b568f8c96
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-02-17 14:17:44 -0500

    palindrome done

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -2,7 +2,31 @@ package com.zipcodewilmington.assessment1.part5;
 
 public class Palindrome {
 
+    //abba
+
+    public static int isPalindromic(String a) {
+        for(int i=0;i<a.length();i++)
+        {
+            if(a.charAt(i)!=a.charAt(a.length()-1-i))
+                return 0;
+        }
+        return 1;
+
+    }
+
     public Integer countPalindromes(String input){
-        return null;
+
+        String a;
+        int countSubs=input.length();
+        for(int i=0;i<input.length();i++)
+        {
+            for(int j=i+2;j<=input.length();j++)
+            {
+                a=input.substring(i,j);
+                countSubs+=isPalindromic(a);
+            }
+        }
+        return countSubs;
+
     }
 }
