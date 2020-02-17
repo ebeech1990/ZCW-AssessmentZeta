c50858fc324f8ce1aeee2f036aa22fb6b1fcfe44
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-02-17 16:21:58 -0500

    part 3 not done

--- src/main/java/com/zipcodewilmington/assessment1/part3/Pet.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/Pet.java
@@ -1,76 +1,77 @@
-package com.zipcodewilmington.assessment1.part3;
-
-/**
- * Created by leon on 2/16/18.
- */
-public abstract class Pet implements Animal {
-    /**
-     * nullary constructor
-     * by default, pet has age of 0; name of "";
-     */
-    public   String name;
-    public Integer age;
-    public PetOwner owner;
-    public Pet() {
-        this.name = "";
-        this.age = 0;
-    }
-
-    /**
-     * @param name name of this pet
-     */
-    public Pet(String name) {
-        this.name = name;
-        this.age = 0;
-    }
-
-
-    /**
-     * @param age age of this pet
-     */
-    public Pet(int age) {
-        this.name = "";
-        this.age = age;
-    }
-
-    /**
-     * @param name name of this pet
-     * @param age age of this pet
-     */
-    public Pet(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    /**
-     * @return name of this pet
-     */
-    public String getName() {
-
-        return name;
-    }
-
-    /**
-     * @return age of this pet
-     */
-    public Integer getAge() {
-
-        return age;
-    }
-
-    /**
-     * @param newPetOwner the new owner of this pet
-     * ensure this instance of `Pet` is added to the owner's composite `pets` list
-     */
-    public void setOwner(PetOwner newPetOwner) {
-        this.owner = newPetOwner;
-    }
-
-    /**
-     * @return PetOwner object whose composite `pets` collection contains this Pet instance
-     */
-    public PetOwner getOwner() {
-
-        return owner;
-    }
-}
+//package com.zipcodewilmington.assessment1.part3;
+//
+///**
+// * Created by leon on 2/16/18.
+// */
+//public abstract class Pet implements Animal {
+//    /**
+//     * nullary constructor
+//     * by default, pet has age of 0; name of "";
+//     */
+//    public   String name;
+//    public Integer age;
+//    public PetOwner owner;
+//
+//    public Pet() {
+//        this.name = "";
+//        this.age = 0;
+//    }
+//
+//    /**
+//     * @param name name of this pet
+//     */
+//    public Pet(String name) {
+//        this.name = name;
+//        this.age = 0;
+//    }
+//
+//
+//    /**
+//     * @param age age of this pet
+//     */
+//    public Pet(int age) {
+//        this.name = "";
+//        this.age = age;
+//    }
+//
+//    /**
+//     * @param name name of this pet
+//     * @param age age of this pet
+//     */
+//    public Pet(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    /**
+//     * @return name of this pet
+//     */
+//    public String getName() {
+//
+//        return name;
+//    }
+//
+//    /**
+//     * @return age of this pet
+//     */
+//    public Integer getAge() {
+//
+//        return age;
+//    }
+//
+//    /**
+//     * @param newPetOwner the new owner of this pet
+//     * ensure this instance of `Pet` is added to the owner's composite `pets` list
+//     */
+//    public void setOwner(PetOwner newPetOwner) {
+//        this.owner = newPetOwner;
+//    }
+//
+//    /**
+//     * @return PetOwner object whose composite `pets` collection contains this Pet instance
+//     */
+//    public PetOwner getOwner() {
+//
+//        return owner;
+//    }
+//}
