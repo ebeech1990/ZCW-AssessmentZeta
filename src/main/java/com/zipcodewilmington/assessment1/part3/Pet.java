288a57bec8174b3e0d42e1d941071cffb84de745
ebeech <31962702+ebeech1990@users.noreply.github.com>
2020-02-18 11:01:57 -0500

    Update Pet.java

--- src/main/java/com/zipcodewilmington/assessment1/part3/Pet.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/Pet.java
@@ -8,13 +8,14 @@ public abstract class Pet implements Animal {
      * nullary constructor
      * by default, pet has age of 0; name of "";
      */
-    public   String name;
-    public Integer age;
-    public PetOwner owner;
+    private String name;
+    private Integer age;
+    private PetOwner owner;
 
     public Pet() {
-        this.name = "";
         this.age = 0;
+        this.name = "";
+
     }
 
     /**
@@ -22,7 +23,7 @@ public abstract class Pet implements Animal {
      */
     public Pet(String name) {
         this.name = name;
-        this.age = 0;
+
     }
 
 
@@ -30,7 +31,7 @@ public abstract class Pet implements Animal {
      * @param age age of this pet
      */
     public Pet(int age) {
-        this.name = "";
+
         this.age = age;
     }
 
@@ -64,7 +65,7 @@ public abstract class Pet implements Animal {
      * ensure this instance of `Pet` is added to the owner's composite `pets` list
      */
     public void setOwner(PetOwner newPetOwner) {
-        this.owner = newPetOwner;
+        owner = newPetOwner;
     }
 
     /**
